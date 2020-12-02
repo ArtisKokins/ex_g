b73f5e84e400a4645c1b38e4d0de84e0d976f52a
Artis Kokins <kokinsartis@gmail.com>
2020-12-02 17:58:55 +0200

    Sub class 2c

---
+++ Om2c.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KDv2_Hierarhija
+{
+    public class Om2c: Om
+    {
+        private double r1, r2;
+        public double R1
+        {
+            set { r1 = value; r = r1 + r2; i = u / r; }
+            get { return r1; }
+        }
+        public double R2
+        {
+            set { r2 = value; r = r1 + r2; i = u / r; }
+            get { return r2; }
+        }
+        public Om2c(double aU, double aR1, double aR2)
+        {
+            u = aU; r1 = aR1; r2 = aR2; r = r1 + r2; i = u / r;
+        }
+    }
+}
